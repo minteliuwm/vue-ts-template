874556f268e0477c13723ab72a41445cd0463839
liuweimin <liuweimin@corp.netease.com>
2020-03-16 10:38:54 +0800

    feat: 新增日志脚本&上线脚本

---
+++ build/release.sh
@@ -0,0 +1,27 @@
+git checkout master
+git merge develop
+
+#!/usr/bin/env sh
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # build
+  npm version $VERSION --no-git-tag-version --allow-same-version
+
+  # commit
+  git tag v$VERSION
+  npm run changelog
+  git commit -am "release: $VERSION"
+
+  # publish
+  git push origin master
+  git push origin refs/tags/v$VERSION
+  git checkout develop
+  git rebase master
+  git push origin develop
+fi
